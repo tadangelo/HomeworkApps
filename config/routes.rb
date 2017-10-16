5a54a46c90f3724ce351bfbfbec3599d3121352a
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-10-16 18:04:01 -0500

    Created a rails app as assigned on 10/16/17

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+ 
+
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+  resources :blogs
+
+  root to: 'pages#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
